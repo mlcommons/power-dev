1df77a54598bf97d13603b995f5ea80324b953ec
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-04-12 15:02:48 +0100

    Fixes formatting

--- compliance/check.py
+++ compliance/check.py
@@ -68,7 +68,7 @@ COMMON_ERROR_RANGING = [
     "Bad watts reading nan from ",
     "Bad amps reading nan from ",
     "Bad pf reading nan from ",
-    "Bad volts reading nan from "
+    "Bad volts reading nan from ",
 ]
 COMMON_ERROR_TESTING = ["USB."]
 
@@ -327,10 +327,10 @@ def phases_check(
         ), f"Phases amount is not equal for {mode} mode."
         for i in range(len(phases_client)):
             time_difference = abs(phases_client[i][0] - phases_server[i][0])
-            assert (
-                time_difference <= 0.5
-            ), f"The time difference for {i + 1} phase of {mode} mode is more than 500ms."\
+            assert time_difference <= 0.5, (
+                f"The time difference for {i + 1} phase of {mode} mode is more than 500ms."
                 f"Observed difference is {time_difference * 1000}ms"
+            )
 
     comapre_time(phases_ranging_c, phases_ranging_s, RANGING_MODE)
     comapre_time(phases_testing_c, phases_testing_s, TESTING_MODE)
@@ -338,11 +338,11 @@ def phases_check(
     def compare_duration(range_duration: float, test_duration: float) -> None:
         duration_diff = (range_duration - test_duration) / range_duration
 
-        assert (
-            duration_diff < 0.05
-        ), f"Duration of the testing mode ({round(test_duration,2)}) is lower than that of "\
-                f"ranging mode ({round(range_duration,2)}) by {round(duration_diff*100,2)} "\
-                f"percent which is more than the allowed 5 percent limit."
+        assert duration_diff < 0.05, (
+            f"Duration of the testing mode ({round(test_duration,2)}) is lower than that of "
+            f"ranging mode ({round(range_duration,2)}) by {round(duration_diff*100,2)} "
+            f"percent which is more than the allowed 5 percent limit."
+        )
 
     def compare_time_boundaries(
         begin: float, end: float, phases: List[Any], mode: str
@@ -536,12 +536,9 @@ def check_ptd_logs(
                 ), f"{line.strip()!r} in ptd_log.txt"
 
                 # Treat uncommon errors in ranging phase as warnings
-                if (
-                    all (
-                        not problem_line.group(0)
-                            .strip()
-                            .startswith(common_ranging_error) for common_ranging_error in COMMON_ERROR_RANGING
-                    )
+                if all(
+                    not problem_line.group(0).strip().startswith(common_ranging_error)
+                    for common_ranging_error in COMMON_ERROR_RANGING
                 ):
                     raise CheckerWarning(
                         f"{line.strip()!r} in ptd_log.txt during ranging stage. Treated as WARNING"
