8dd39bec6150e0cfeefb1b51a84f76ec6cc8cb4f
dmiskovic-NV <dmiskovic@nvidia.com>
2023-11-21 15:24:18 -0800

    fixed formatting

--- ptd_client_server/lib/server.py
+++ ptd_client_server/lib/server.py
@@ -305,9 +305,9 @@ class ServerConfig:
         )
         self.ptd_device_type: int = get("ptd", "deviceType", parse=int)
         if self.ptd_device_type in DC_DEVICES:
-            ptd_dc_flag  = str("-D")
+            ptd_dc_flag = str("-D")
         else:
-            ptd_dc_flag  = None
+            ptd_dc_flag = None
         ptd_interface_flag: str = get("ptd", "interfaceFlag")
         ptd_device_port: str = get("ptd", "devicePort")
         ptd_board_num: Optional[int] = get("ptd", "gpibBoard", parse=int, fallback=None)
