3b6b63257181d101aca72e9a37a042b4c922ea5b
Arjun Suresh <arjunsuresh1987@gmail.com>
2023-11-27 22:16:20 +0530

    Fix formatting

--- compliance/check.py
+++ compliance/check.py
@@ -396,8 +396,8 @@ def phases_check(
         )
 
         # convert to UTC
-        power_begin = datetime.fromtimestamp(power_begin, tz = timezone.utc)
-        power_end = datetime.fromtimestamp(power_end, tz = timezone.utc)
+        power_begin = datetime.fromtimestamp(power_begin, tz=timezone.utc)
+        power_end = datetime.fromtimestamp(power_end, tz=timezone.utc)
 
         detail_log_fname = os.path.join(run_path, "mlperf_log_detail.txt")
         datetime_format = "%m-%d-%Y %H:%M:%S.%f"
@@ -657,12 +657,12 @@ def check_ptd_logs(
             continue
         if (not start_ranging_time) and (start_ranging_line == msg):
             start_ranging_time = get_time_from_line(
-                line, date_regexp, file_path, 0 #timezone_offset
+                line, date_regexp, file_path, 0 # timezone_offset
             )
         if (not stop_ranging_time) and bool(start_ranging_time):
             if ": Completed test" == msg:
                 stop_ranging_time = get_time_from_line(
-                    line, date_regexp, file_path, 0 #timezone_offset
+                    line, date_regexp, file_path, 0 # timezone_offset
                 )
                 break
 
@@ -677,7 +677,7 @@ def check_ptd_logs(
             try:
                 log_time = None
                 log_time = get_time_from_line(
-                    line, date_regexp, file_path, 0 #timezone_offset
+                    line, date_regexp, file_path, 0 # timezone_offset
                 )
             except LineWithoutTimeStamp:
                 assert (
